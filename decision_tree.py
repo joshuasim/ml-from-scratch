9ed83b570ce6bea12837278d9860fce02a2c53bf
joshuasim <joshuasim800@gmail.com>
2026-06-17 14:25:54 -0700

    WIP: decision tree, fit function

--- decision_tree.py
+++ decision_tree.py
@@ -8,7 +8,23 @@ class decision_tree():
         self.L_leaf = None
         self.R_leaf = None
 
-    def fit():
+    def fit(self, x, y):
+        best_gini = float('inf')
+        for feature_idx in range(x.shape[1]):
+            thresholds = np.unique(x[:, feature_idx])
+            for i in thresholds:
+                left_y = y[x[:, feature_idx] <= i]
+                right_y = y[x[:, feature_idx] > i]
+                gini_left = self.gini(left_y)
+                gini_right = self.gini(right_y)
+                weighted_gini = (left_y.shape[0]/y.shape[0]) * gini_left + (right_y.shape[0]/y.shape[0])*gini_right
+                if weighted_gini < best_gini:
+                best_gini = weighted_gini
+                self.feature = x[feature_idx]
+                self.threshold = i
+                self.L_leaf = idk
+                self.R_leaf = idk
+
         def gini(self, y):
             prob_1 = np.sum(y)/y.shape[0]
             prob_0 = 1 - prob_1
