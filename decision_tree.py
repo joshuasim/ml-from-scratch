fc0f4e03e440b01e594a09a7167fe52c610151c4
joshuasim <joshuasim800@gmail.com>
2026-06-18 14:08:17 -0700

    complete depth-1 decision tree stump: test against sklearn, 0.903 accuracy

--- decision_tree.py
+++ decision_tree.py
@@ -8,8 +8,9 @@ class decision_tree():
         self.L_leaf = None
         self.R_leaf = None
 
+    # iterates through features and thresholds to set the feature index and threshold for the depth-1 tree stump and calculates the left and right leaf.
     def fit(self, x, y):
-        best_gini = float('inf')
+        best_gini = float('inf') # ensures any weight_gini is less than the initial best_gini
         for feature_idx in range(x.shape[1]):
             thresholds = np.unique(x[:, feature_idx])
             for i in thresholds:
@@ -18,15 +19,47 @@ class decision_tree():
                 gini_left = self.gini(left_y)
                 gini_right = self.gini(right_y)
                 weighted_gini = (left_y.shape[0]/y.shape[0]) * gini_left + (right_y.shape[0]/y.shape[0])*gini_right
-                if weighted_gini < best_gini:
-                best_gini = weighted_gini
-                self.feature = x[feature_idx]
-                self.threshold = i
-                self.L_leaf = idk
-                self.R_leaf = idk
+                if weighted_gini < best_gini: # changes best gini and sets the left leaf, right leaf, feature and threshold only when new calculated weighted gini is less than previous best gini
+                    best_gini = weighted_gini
+                    if int(np.sum(left_y) / len(left_y) >= 0.5): # assigns leaf predictions based on majority class in each split group.
+                        self.L_leaf = 1
+                        self.R_leaf = 0
+                    else:
+                        self.R_leaf = 1
+                        self.L_leaf = 0
+                    self.feature = feature_idx
+                    self.threshold = i        
 
-        def gini(self, y):
-            prob_1 = np.sum(y)/y.shape[0]
-            prob_0 = 1 - prob_1
-            impurity = 1 - (prob_0**2 + prob_1**2)
-            return impurity
+    # gini method to calcuate gini impurity
+    def gini(self, y):
+        if len(y) == 0: # this ensures no RuntimeWarning for invalid value encontered for division with 0
+            return 0
+        prob_1 = np.sum(y)/y.shape[0]
+        prob_0 = 1 - prob_1
+        impurity = 1 - (prob_0**2 + prob_1**2)
+        return impurity
+    
+    # returns predicted class label for each sample based on the learned feature and threshold.
+    def predict(self, x):
+        return np.where(x[:, self.feature] <= self.threshold, self.L_leaf, self.R_leaf)
+
+# dummy dataset to evaluate. 
+from sklearn.datasets import make_classification
+from sklearn.tree import DecisionTreeClassifier 
+x, y = make_classification(n_samples=1000, n_features=2, n_redundant=0, n_informative=2, n_clusters_per_class=1, random_state=42)
+
+# call my implementation 
+tree = decision_tree()
+tree.fit(x, y)
+pred = tree.predict(x)
+accuracy = np.mean(pred == y)
+print(f'scratch model accuracy: {accuracy}')
+# 0.903
+
+# call sklearn tree implementation at depth 1
+sklearn_tree = DecisionTreeClassifier(max_depth=1)
+sklearn_tree.fit(x, y)
+pred_sklearn = sklearn_tree.predict(x)
+sklearn_tree_accuracy = np.mean(pred_sklearn == y)
+print(f'sklearn model accuracy: {sklearn_tree_accuracy}')
+# 0.903
